d58293973b0f765d7b8774a1c8def21bbf43f6e7
Некит программист < @DESKTOP-0N9MUS7>
2023-04-02 00:19:07 +0700

    create orm

---
+++ energy_gym_authserver/orm/__init__.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import create_session
+
+from ..configmodule import config
+
+
+engine = create_engine(config.database.connection_string, future=True)
+Base = declarative_base()
+
+
+def session_factory(**kwargs):
+    return create_session(
+        bind=engine,
+        autocommit=False,
+        autoflush=False,
+        future=True,
+        **kwargs
+    )
+
+
+from .user import User
---
+++ energy_gym_authserver/orm/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy import Boolean
+
+from . import Base
+
+
+class User(Base):
+
+    __tablename__ = 'users'
+
+    id            = Column(Integer, primary_key=True, autoincrement=True)
+    student_card  = Column(Integer, nullable=False, unique=True)
+    name          = Column(String(70), nullable=False)
+    group         = Column(String(20), nullable=False)
+    password      = Column(String(60), nullable=False)
+    role          = Column(String(15), nullable=False)
+    deleted       = Column(Boolean, default=False)
