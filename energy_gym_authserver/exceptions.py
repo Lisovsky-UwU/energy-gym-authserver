24bbbb61b5caff82250092c8697e90fcbe0fdc9d
Некит программист < @DESKTOP-0N9MUS7>
2023-04-02 21:55:40 +0700

    modify exceptions

--- energy_gym_authserver/exceptions.py
+++ energy_gym_authserver/exceptions.py
@@ -1,19 +1,33 @@
 class EnergyGymAuthServerException(Exception):
     '''Базовое исключение сервера'''
 
+
 class DataBaseException(EnergyGymAuthServerException):
     '''Исключение при при ошибке в работе с базой данных'''
+
 
 class MainServerRequestException(EnergyGymAuthServerException):
     '''Исключение при возвращении ошибки от главного сервера'''
 
+    def __init__(self, status_code: int = 500, *args: object) -> None:
+        super().__init__(*args)
+        self.status_code = status_code
+
+
 class InvalidRequestException(EnergyGymAuthServerException):
     '''Исключение при неверном запросе'''
+
 
 class LoginError(EnergyGymAuthServerException):
     '''Исключение при ошибке входа'''
     status_code = 401
 
+
 class RegistrationError(EnergyGymAuthServerException):
     '''Исключение при ошибке регистрации'''
     status_code = 401
+
+
+class AccessRightsException(EnergyGymAuthServerException):
+    '''Исключение при недостаточных правах доступа'''
+    status_code = 403
