886137a37f9fc0cc282351e007d1439abf9f3339
Lisovsky_UwU <nikish10012@gmail.com>
2024-04-19 18:35:14 +0700

    Update user orm model

--- energy_gym_authserver/orm/user.py
+++ energy_gym_authserver/orm/user.py
@@ -1,22 +1,24 @@
 from typing import Any, Dict, Optional
-from sqlalchemy import Column, String, Integer, Boolean, Text
+from sqlalchemy import Enum
+from sqlalchemy.orm import Mapped, mapped_column
 
 from . import Base
+from ..models import UserRole
 
 
 class User(Base):
 
     __tablename__ = 'users'
 
-    id            = Column(Integer, primary_key=True, autoincrement=True)
-    student_card  = Column(Integer, nullable=False, unique=True)
-    firstname     = Column(String(30), nullable=False)
-    secondname    = Column(String(30), nullable=False)
-    surname       = Column(String(30), nullable=False, default='')
-    group         = Column(String(20), nullable=False)
-    hid           = Column(Text, nullable=False)
-    role          = Column(String(15), nullable=False, index=True)
-    deleted       = Column(Boolean, default=False)
+    id           : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    student_card : Mapped[int] = mapped_column(unique=True, index=True)
+    firstname    : Mapped[str]
+    secondname   : Mapped[str]
+    surname      : Mapped[str]
+    group        : Mapped[str]
+    hid          : Mapped[str]
+    role         : Mapped[UserRole] = mapped_column(Enum(UserRole))
+    deleted      : Mapped[bool] = mapped_column(server_default='false')
 
 
     def to_dict(
