75fb1437d5cd10c59bc956fc2b193f5443431e74
Lisovsky_UwU <nikish10012@gmail.com>
2024-06-07 03:23:26 +0700

    Update static

--- static/assets/Loading-9088dda3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{a as _,c as o}from"./index-61c05336.js";const c={},s={class:"loader"};function t(n,a){return _(),o("div",s)}const i=e(c,[["render",t],["__scopeId","data-v-3f44124e"]]);export{i as L};
